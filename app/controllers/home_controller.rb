d518c1258ddaa6dce5770aa3271272d91b6e1fd9
pasha <p.korenev@voroninstudio.eu>
2014-05-30 12:50:51 +0300

    button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
+    str = css_svg('static_images/all/Launguage-EN color.svg')
+    base64 = Base64.encode64( str )
+    result = base64
+    #render inline: result #str.delete("\n")
   end
+
+  def css_svg(filename, options={})
+  file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+  doc = Nokogiri::HTML::DocumentFragment.parse file
+  svg = doc.at_css 'svg'
+  if options[:class].present?
+    svg['class'] = options[:class]
+  end
+  #doc.to_html.
+  svg.to_s
+end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,38 @@
 module ApplicationHelper
+  def brand_wrap(title, tag = 'span', link = false)
+    #render template: 'helpers/application/brand_wrap'
+    render partial: 'helpers/application/brand_wrap', locals: { title: title, tag: tag, link: link }
+  end
+
+  def embedded_svg filename, options={}
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    doc.to_html.html_safe
+  end
+
+  # def self.css_svg(filename, options={})
+  #   file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+  #   doc = Nokogiri::HTML::DocumentFragment.parse file
+  #   svg = doc.at_css 'svg'
+  #   if options[:class].present?
+  #     svg['class'] = options[:class]
+  #   end
+  #   #doc.to_html.
+  #   svg.to_s
+  # end
+
+  def self.css_svg(filename, options={})
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    #doc.to_html.
+    svg.to_s
+  end
 end
