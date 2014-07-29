e55badfef6bf1973076a247643299ba94727a842
pasha <p.korenev@voroninstudio.eu>
2014-07-29 16:41:40 +0300

    fix

--- db/migrate/20140729133336_add_title_to_about_page_block_translation.rb
+++ db/migrate/20140729133336_add_title_to_about_page_block_translation.rb
@@ -1,6 +1,6 @@
 class AddTitleToAboutPageBlockTranslation < ActiveRecord::Migration
   def up
-    rename_column AboutPageBlock.translation_model.table_name, :title, :image_title
-    add_column AboutPageBlock.translation_model.table_name, :title, :string
+    rename_column AboutPageBlock.translation_class.table_name, :title, :image_title
+    add_column AboutPageBlock.translation_class.table_name, :title, :string
   end
 end
